d9fd1169bf29db91b9c033a458d8cf072bbe2e95
Usman Ahmad <hafizusman@gosign.de>
2012-08-24 18:46:32 +0500

    first version of the basic iOS app
    this is the first version of the basic iOS app which contains some
    gesture recognition like pan and pinch and having a very simple model,
    have a custom view for a basic drawing of circles and somewhat arcs

---
+++ Happiness/happinessAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  happinessAppDelegate.h
+//  Happiness
+//
+//  Created by Usman Ahmad on 8/24/12.
+//  Copyright (c) 2012 Upanels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface happinessAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Happiness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Happiness
+//
+//  Created by Usman Ahmad on 8/24/12.
+//  Copyright (c) 2012 Upanels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "happinessAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([happinessAppDelegate class]));
+    }
+}
